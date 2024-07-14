b3d07bb4ac580e20c3f8135580c6b98ad67f4b62
Developia <opiaaustin@gmail.com>
2024-07-14 11:00:35 +0100

    Are you sure delete function

--- resources/views/auth/products/index.blade.php
+++ resources/views/auth/products/index.blade.php
@@ -49,7 +49,7 @@
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                         <a href="{{ route('products.edit', $product->id) }}" class="text-indigo-600 dark:text-indigo-400 hover:text-indigo-900">Edit</a>
-                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="inline-block">
+                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="inline-block" onsubmit="return confirm('Do you really want to delete this product?');">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="text-red-600 dark:text-red-400 hover:text-red-900 ml-4">Delete</button>
